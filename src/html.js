dc60d7b7353b86e3dfe374ea6c2822ae43a192fd
Mohamad Mohebifar <netfars@gmail.com>
2017-12-04 17:11:26 -0330

    fix: Add favicon

--- src/html.js
+++ src/html.js
@@ -44,7 +44,9 @@ class Html extends Component {
             name="viewport"
             content="width=device-width, initial-scale=1.0"
           />
-          <title>Gatsby - Redux</title>
+          <link rel="shortcut icon" href="/favicons/favicon.ico" type="image/x-icon" />
+          <link rel="icon" href="/favicons/favicon-16x16.png" sizes="16x16" type="image/png" />
+          <link rel="icon" href="/favicons/favicon-32x32.png" sizes="32x32" type="image/png" />
         </head>
         <body>
           <div
